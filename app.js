aae86cb798722092f3f205bb96cac40fe7919381
Sahil <130554410+sahil3241@users.noreply.github.com>
2024-01-21 22:32:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const tasks = require('./routes/tasks');
 
 const connectDB = require('./db/connect');
 
-const path = require('path');
-
 require('dotenv').config();
 
 const notFound = require('./middleware/not-found');
@@ -19,13 +17,8 @@ app.use(express.json());
 app.use(express.static('./public'));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, './public')))
 
 // routes
-//rest api
-app.use('*', function(req, res){
-    res.sendFile(path.join(__dirname, './public/index.html'))
-})
 app.use('/api/v1/tasks', tasks);
 
 app.use(notFound);
